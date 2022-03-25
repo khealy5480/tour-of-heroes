4173fa629caeb4c51efc4caf62c11d631cbee257
Skipuavforge <kevinsonlineshopping@verizon.net>
2022-03-25 16:31:23 -0400

    Don't know where I am.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +8,8 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+    heroes = HEROES;
+    hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
@@ -15,6 +17,10 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
